ff6749d5b088d1fa281b714907490a5db4207544
V.M.BINTANG <ytvallenz@gmail.com>
2024-02-16 15:48:04 +0700

    create table

---
+++ app/Http/Controllers/StuffsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\stuffs;
+use App\Http\Requests\StorestuffsRequest;
+use App\Http\Requests\UpdatestuffsRequest;
+
+class StuffsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorestuffsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(stuffs $stuffs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(stuffs $stuffs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatestuffsRequest $request, stuffs $stuffs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(stuffs $stuffs)
+    {
+        //
+    }
+}
